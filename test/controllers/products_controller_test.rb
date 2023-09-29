2b30c717ef9640dd0c4f4608c309f2339603ea52
Guillermo Echague <guillermo.echaguea@gmail.com>
2023-09-29 01:21:15 -0300

    App work before delete 19 video

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,6 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
 
-
   test 'render a list of products' do 
     get products_path
     assert_response :success
@@ -16,4 +15,59 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.description', 'PS4 en buen estado'
     assert_select '.price', '$150'
   end
+
+  test 'render a new product form' do
+    get new_product_path
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'Nintendo 64',
+        description: 'Le faltan los cables',
+        price: 45
+      }
+    }
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha creado correctamente.'
+  end
+
+  test 'does not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'Le faltan los cables',
+        price: 45
+      }
+    }
+    assert_response :unprocessable_entity
+  end
+
+  test 'render a edit product form' do
+    get edit_product_path(products(:ps4))
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to update a product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: 165
+      }
+    }
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha actualizado correctamente.'
+  end
+
+  test 'Does not allow to update a product with an invalid field' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: nil
+      }
+    }
+    assert_response :unprocessable_entity
+  end
+
 end
