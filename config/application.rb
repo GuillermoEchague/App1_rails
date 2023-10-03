82f648470755d14693d6ee6ff45ff3f662557da1
Guillermo Echague <guillermo.echaguea@gmail.com>
2023-10-03 14:27:49 -0300

    Add multilenguage

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # Available languagues
+    config.i18n.available_locales = [:en, :es] 
+
+    # lenguage by default
+    config.i18n.default_locale = :es
   end
 end
